43ff6a6d6354ccf557086660be077d67ea217caa
Preethi Dhakshinamoorthy <p.dhakshinamoorthy@perficient.com>
2016-06-03 18:28:41 +0530

    Added a Textbox

---
+++ src/main/resources/Test.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test {
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+
+		WebElement username = driver.findElement(By.Name("username"));
+		username.sendKeys("zzz");
+	}
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 <h2>Hello World again!</h2>
+Username: <input type="text" name="username">
 </body>
 </html>
